a034fda9c21e7faf4252948cdfc58c51555903f9
Ibrahim Cesar <email@ibrahimcesar.com>
2022-03-09 07:27:25 -0300

    add: variables to work with lambda on amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    REVUE_API_KEY: process.env.REVUE_API_KEY,
+    NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
+    SCORE: process.env.SCORE
+  }
 }
 
 module.exports = nextConfig
